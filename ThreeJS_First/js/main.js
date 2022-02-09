1d1d5d17e7dbaca7caf41bdd012bc4e17d3f6981
Tathagata Guha Ray <nguharay71@gmail.com>
2022-02-09 23:07:22 +0530

    Working commit, new threejs project underway

--- ThreeJS_First/js/main.js
+++ ThreeJS_First/js/main.js
@@ -5,33 +5,40 @@
 
 
 
-// import * as THREE from './three.js'
-// import {OrbitControls} from './node_modules/three/examples/jsm/controls/OrbitControls'
+// import * as THREE from './three'
+import {OrbitControls} from '//cdn.skypack.dev/three@0.134/examples/jsm/controls/OrbitControls?min'
+
+// const canvas = document.querySelector('.webgl')
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.TorusGeometry(12, 3, 15, 500);
-const material = new THREE.MeshStandardMaterial( { color: 0xff00ff} );
-const torus = new THREE.Mesh( geometry, material );
+const geometry = new THREE.TorusGeometry(12, 3, 15, 400);
+const material = new THREE.MeshStandardMaterial( { color: 0xFFFFFF, wireframe:false, antialias:true } );
+const torus = new THREE.Mesh(geometry, material);
 scene.add(torus);
 
-const pointLight = new THREE.PointLight(0xFF00);
-pointLight.position.set(15,0,10)
+const pointLight = new THREE.PointLight(0x00FFFF);
+pointLight.position.set(20,10,20)
 
-const ambientLight = new THREE.AmbientLight(0x66aa88)
-
+const ambientLight = new THREE.AmbientLight(0xAA1154)
 scene.add(pointLight, ambientLight)
 
 const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50, 0x00ff00)
-scene.add(lightHelper, gridHelper)
 
-// const controls = new OrbitControls(camera, renderer.domElement);
+const gridHelper = new THREE.GridHelper(200, 50, 0x00FF00)
+scene.add(lightHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 camera.position.z = 30;
 
@@ -40,7 +47,13 @@ function animate() {
     torus.rotation.x += 0.005;
     torus.rotation.y += 0.005;
     torus.rotation.z += 0.005;
-    // controls.update();
+    controls.update();
 	renderer.render(scene, camera);
 }
 animate();
+
+window.addEventListener('resize', function(){
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight)
+});
